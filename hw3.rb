6cc1897cca7d84fececbe992beed5d228bb25f1f
Adam Kelsey <adam.kelsey113@gmail.com>
2015-01-07 15:10:50 -0500

    commiting higher/lower homework

---
+++ hw3.rb
@@ -0,0 +1,20 @@
+number_range = rand(0..100)
+turns = 0
+user_guess = false
+
+while !user_guess && turns < 5
+  puts "Guess a number between 1 and 100"
+  guess = gets.chomp.to_i
+  number = guess.to_i
+  if number == number_range
+    puts "You guessed correct!"
+    user_guess = true
+    break
+  elsif guess > number_range
+      puts "Guess Lower!"
+      turns += 1
+  elsif guess < number_range
+      puts "Guess Higher!"
+      turns += 1
+  end
+end
